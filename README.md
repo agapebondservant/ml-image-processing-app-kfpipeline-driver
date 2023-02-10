5782bbd6d2c98b37088b532c591bc561b048e3b3
Tola Awofolu <oawofolu@pivotal.io>
2023-02-09 19:12:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+## Deployment
+* Create a Docker secret for the target container registry:
+```
+source .env
+REGISTRY_PASSWORD=$DATA_E2E_REGISTRY_PASSWORD kp secret create tbs-demo-docker-secret --dockerhub ${DATA_E2E_REGISTRY_USERNAME}
+```
+
+* Create a Git secret for the source git repository:
+```
+source .env
+GIT_PASSWORD=$DATA_E2E_GITHUB_PW kp secret create tbs-demo-git-secret --git-url https://github.com --git-user ${DATA_E2E_REGISTRY_USERNAME}
+```
+
+* Build the images via manifest:
+```
+source .env
+envsubst < config/ml-image-processing-container.in.yaml > config/ml-image-processing-container.yaml
+kubectl apply -f config/ml-image-processing-container.yaml
+```
+
+* Tail the logs: (must install kp cli - see <a href="https://github.com/vmware-tanzu/kpack-cli/blob/v0.2.0/docs/kp.md">link</a>)
+```
+kp build logs imgprocessor-kfp
+```
+
+* Build the images via kp (alternative approach):
+```
+source .env
+kp image create imgprocessor-kfp --tag ${DATA_E2E_REGISTRY_USERNAME}/ml-image-processor-kfp  \
+        --namespace default \
+        --wait \
+        --env MLFLOW_TRACKING_URI=http://mlflow.${DATA_E2E_BASE_URL} \
+        --env MLFLOW_S3_ENDPOINT_URL=http://minio-ml.${DATA_E2E_BASE_URL} \
+        --git “https://github.com/agapebondservant/ml-image-processing-app-kfpipeline-driver.git” \
+        --git-revision "main"
+```
+
+* Build the images via docker (alternative approach):
+```
+source .env
+docker build --build-arg MLFLOW_TRACKING_URI_VAL=http://mlflow.${DATA_E2E_BASE_URL} \
+             --build-arg MLFLOW_S3_ENDPOINT_URL_VAL=http://minio-ml.${DATA_E2E_BASE_URL} \
+             -t ${DATA_E2E_REGISTRY_USERNAME}/ml-image-processor-kfp .
+docker push ${DATA_E2E_REGISTRY_USERNAME}/ml-image-processor-kfp
+```
+
+To delete the image:
+```
+kubectl delete -f config/ml-image-processing-container.yaml
+```
---
+++ app/main.py
@@ -0,0 +1,90 @@
+import kfp.dsl as dsl
+from kfp import Client
+import logging
+import warnings
+from app import utils
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+warnings.filterwarnings('ignore')
+
+
+@dsl.pipeline(
+    name='cifar_cnn_kfp',
+    description='Pipeline which trains and serves a CNN model using Keras.'
+)
+def cifar_pipeline():
+    # Upload Dataset
+    upload_dataset = dsl.ContainerOp(
+        name='upload_dataset',
+        image='oawofolu/ml-image-processor',
+        arguments=[
+            "/app/main.py",
+            "--mlflow_entry", 'upload_dataset',
+            "--mlflow_stage", utils.get_env_var('MLFLOW_STAGE'),
+            "--git_repo", utils.get_env_var('GIT_REPO'),
+            "--experiment_name", utils.get_env_var('EXPERIMENT_NAME'),
+            "--environment_name", utils.get_env_var('ENVIRONMENT_NAME')
+        ]
+    )
+
+    # Train Model
+    train_model = dsl.ContainerOp(
+        name='train_model',
+        image='oawofolu/ml-image-processor',
+        arguments=[
+            "/app/main.py",
+            "--mlflow_entry", 'train_model',
+            "--mlflow_stage", utils.get_env_var('MLFLOW_STAGE'),
+            "--git_repo", utils.get_env_var('GIT_REPO'),
+            "--experiment_name", utils.get_env_var('EXPERIMENT_NAME'),
+            "--environment_name", utils.get_env_var('ENVIRONMENT_NAME')
+        ]
+    )
+    train_model.after(upload_dataset)
+
+    # Evaluate Model
+    evaluate_model = dsl.ContainerOp(
+        name='evaluate_model',
+        image='oawofolu/ml-image-processor',
+        arguments=[
+            "/app/main.py",
+            "--mlflow_entry", 'evaluate_model',
+            "--mlflow_stage", utils.get_env_var('MLFLOW_STAGE'),
+            "--git_repo", utils.get_env_var('GIT_REPO'),
+            "--experiment_name", utils.get_env_var('EXPERIMENT_NAME'),
+            "--environment_name", utils.get_env_var('ENVIRONMENT_NAME')
+        ]
+    )
+    evaluate_model.after(train_model)
+
+    # Promote Model to Staging
+    promote_model_to_staging = dsl.ContainerOp(
+        name='promote_model_to_staging',
+        image='oawofolu/ml-image-processor',
+        arguments=[
+            "/app/main.py",
+            "--mlflow_entry", 'promote_model_to_staging',
+            "--mlflow_stage", utils.get_env_var('MLFLOW_STAGE'),
+            "--git_repo", utils.get_env_var('GIT_REPO'),
+            "--experiment_name", utils.get_env_var('EXPERIMENT_NAME'),
+            "--environment_name", utils.get_env_var('ENVIRONMENT_NAME')
+        ]
+    )
+    promote_model_to_staging.after(evaluate_model)
+
+    # steps = [upload_dataset, train_model, evaluate_model, promote_model_to_staging]
+    # for step in steps:
+    #    step.apply(onprem.mount_pvc(pvc_name, 'local-storage', '/mnt'))
+
+
+if __name__ == '__main__':
+    logging.info("Compiling Kubeflow pipeline...")
+    import kfp.compiler as compiler
+    compiler.Compiler().compile(cifar_pipeline, __file__ + '.zip')
+
+    logging.info("Generating new experiment run...")
+    client = Client(host=f'{utils.get_env_var("KUBEFLOW_PIPELINES_HOST")}/pipelines')
+    cifar_experiment = client.create_experiment(name=utils.get_env_var('EXPERIMENT_NAME'))
+    cifar_run = client.run_pipeline(cifar_experiment.id, 'cifar-pipeline', __file__ + '.zip')
+    logging.info("Run completed.")
